40ed9aef47e332bf4ab7fa14d2edf30f0ba32ea9
Jamie <>
2018-12-18 14:14:49 +0800

    地图模拟数据

--- src/components/map/mapList.js
+++ src/components/map/mapList.js
@@ -13,14 +13,42 @@ export default class Index extends PureComponent {
 	loadBaiDuMap () {
 		try{
 			console.log(window.BMap);
-			const map = new BMap.Map('mapContainer');
-			// 创建地图实例
+			const liteMap = new BMap.Map('mapContainer');
 			const point = new BMap.Point(116.404, 39.915);
-			// 创建点坐标
-			map.centerAndZoom(point, 11);
+			liteMap.centerAndZoom(point, 11);
+			
 			// 初始化地图， 设置中心点坐标和地图级别
-			const marker = new BMap.Marker(point);
-			map.addOverlay(marker);
+			// const marker = new BMap.Marker(point);
+			// liteMap.addOverlay(marker);
+
+
+			// 获取地图区域的边界
+			const bounds = liteMap.getBounds();
+			// 获取西南角的经纬度信息
+			const sw = bounds.getSouthWest();
+			// 获取东北角的经纬度信息 东边的经度>西边的经度 北边的纬度>南边的纬度
+			const ne = bounds.getNorthEast();
+
+			// lng为longitute 经度 lat为latitute纬度
+			const lngSpan = ne.lng - sw.lng;
+			const latSpan = ne.lat - sw.lat;
+			const arrFeatures = [];
+			for (let i = 0; i < 10; i ++) {
+				// 初始化位置
+				const mpt = new BMap.Point(sw.lng + lngSpan * (Math.random() * 0.7 + 0.15),
+					sw.lat + latSpan * (Math.random() * 0.7 + 0.15));
+				// 变换icon
+				const iconOffsetX = 42;
+				const iconOffsetY = 66;
+				
+				const massFeature = new BMap.MassFeature(mpt, {data: 'MassFeature' + i});
+				arrFeatures.push(massFeature);
+			}
+			
+			setTimeout(()=>{
+				liteMap.addMassFeatures(arrFeatures);
+			},10)
+			
 		}catch (e) {
 			console.log(e);
 		}
